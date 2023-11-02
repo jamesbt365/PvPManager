46198b627c57f8cd2c6711a9d0bdb92079b08c38
jamesbt365 <jamesbt365@gmail.com>
2023-11-02 14:52:08 +0000

    refactor

--- PvPManager/PvPManager.cs
+++ PvPManager/PvPManager.cs
@@ -1,40 +1,58 @@
 ﻿using TerrariaApi.Server;
 using Terraria;
 using TShockAPI;
+using IL.Terraria.GameContent.Dyes;
 
 namespace PvPManager;
 
 [ApiVersion(2, 1)]
-public class PvPManager : TerrariaPlugin
-{
+public class PvPManager: TerrariaPlugin {
   public override string Name => "PvPManager";
   public override Version Version => new Version(0, 1, 0);
   public override string Author => "jamesbt365";
   public override string Description => "A plugin to manage the state of PvP or teams for yourself and the server.";
 
-  public PvPManager(Main game) : base(game) {}
+  public PvPManager(Main game): base(game) {}
 
-  public override void Initialize()
-  {
+  public override void Initialize() {
     // command to switch your team/others teams
     // Make it possible to force pvp on?
     // Command to force teams?
     // command to get all hostile players.
-    // command to view teams.
-    Commands.ChatCommands.Add(new Command("pvpmanager.pvp.enableall", EnablePvPAll, "pvp-all"));
-    Commands.ChatCommands.Add(new Command("permissions.pvp.toggle", TogglePvP, "toggle-pvp"));
+    Commands.ChatCommands.Add(new Command("pvpmanager.pvp.enableall", EnablePvPAll, "pvp-all") {
+        HelpText = "Enable PvP for everybody!"
+    });
+    // Need version to disable.
+    Commands.ChatCommands.Add(new Command("pvpmanager.pvp.toggle", TogglePvP, "toggle-pvp", "pvp-toggle", "tpvp") {
+        HelpText = "Toggle a users PvP status!"
+    });
+    Commands.ChatCommands.Add(new Command("pvpmanager.teams.setall", SetAllTeam, "team-set-all") {
+        HelpText = "Change the team for everybody to a specified team!"
+    });
   }
 
-  private void TogglePvP(CommandArgs args) {
+    private void SetAllTeam(CommandArgs args)
+    {
+        var player = args.Player;
+        if (player == null) {
+            return;
+        }
+
+        foreach(var ply in TShock.Players) {
+            player.SendInfoMessage(ply?.Team.ToString());
+            // 0 to 5?
+        }
+    }
+
+    private void TogglePvP(CommandArgs args) {
     var player = args.Player;
     if (player == null) {
       return;
     }
     var silent = args.Silent;
-    if (silent == true && args.Parameters.Count > 0) {
-      if (!player.HasPermission("pvpmanager.pvp.toggle.silent")) {
+    if (silent && args.Parameters.Count > 0 && !player.HasPermission("pvpmanager.pvp.toggle.silent")) {
         player.SendErrorMessage("You do not have permission to execute this silently.");
-      }
+        return;
     }
 
     if (args.Parameters.Count == 0) {
@@ -43,6 +61,7 @@ public class PvPManager : TerrariaPlugin
       } else {
         player.SetPvP(true);
       }
+      player.SendInfoMessage("Your PvP status has been toggled!");
       return;
     }
 
@@ -60,8 +79,8 @@ public class PvPManager : TerrariaPlugin
           } else {
             player.SetPvP(true);
           }
-          if (silent == false) {
-            matched_players[0].SendInfoMessage("Your PvP status has been toggled!");
+          if (!silent) {
+            matched_players[0].SendInfoMessage("Your PvP status has been toggled for you!");
           }
         }
       } else {
@@ -70,17 +89,16 @@ public class PvPManager : TerrariaPlugin
     }
   }
 
-    public void EnablePvPAll(CommandArgs args)
-  {
+  public void EnablePvPAll(CommandArgs args) {
     var player = args.Player;
     if (player == null) {
       return;
     }
 
-    if (args.Silent == true) {
+    if (args.Silent) {
       if (player.HasPermission("pvpmanager.pvp.enableall.silent")) {
         player.SendSuccessMessage("Enabling PVP for everyone!");
-        foreach (var ply in TShock.Players) {
+        foreach(var ply in TShock.Players) {
           ply?.SetPvP(true);
         }
       } else {
@@ -89,13 +107,12 @@ public class PvPManager : TerrariaPlugin
 
     } else {
       TShock.Utils.Broadcast("Everyones PVP has been enabled!", 255, 255, 0);
-      foreach (var ply in TShock.Players) {
+      foreach(var ply in TShock.Players) {
         ply?.SetPvP(true);
       }
     }
   }
-  protected override void Dispose(bool disposing)
-  {
+  protected override void Dispose(bool disposing) {
     if (disposing) {}
     base.Dispose(disposing);
   }
